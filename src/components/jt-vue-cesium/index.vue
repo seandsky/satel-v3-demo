b281f670f81f43b1229f726105a4521f436c4fa3
tanghaojie <thj_3360016@qq.com>
2021-06-11 17:09:02 +0800

    init

---
+++ src/components/jt-vue-cesium/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    ref="jtVueCesium"
+    class="jt-vue-cesium"
+    style="height: 100vh; width: 100vw"
+  ></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import * as Cesium from 'cesium'
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+
+export default defineComponent({
+  name: 'jt-vue-cesium',
+  mounted() {
+    const container = this.$refs.jtVueCesium as HTMLElement
+    new Cesium.Viewer(container)
+  },
+})
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home w-full h-full">
+    <jt-vue-cesium />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import jtVueCesium from '../components/jt-vue-cesium/index.vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: { jtVueCesium },
+})
+</script>
